554b6f8ab56f47f4d68f44ae2383d8a76d0c0181
Jcarpio <jcarpio1994@hotmail.com>
2023-02-06 19:46:15 -0500

    added new features

--- OpenAI_Api.js
+++ OpenAI_Api.js
@@ -1,17 +1,29 @@
-async function generateResponse() {
-    // Replace the string with your OpenAI API key
-    const apiKey = "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";
-
-    // Define the model and prompt to use
-    const model = "text-davinci-002";
-    const prompt =  document.getElementById("prompt");
-
-    // Generate a response using the OpenAI API
-    const response = await openai.prompt(apiKey, model, prompt);
-
-    // Extract the response text
-    const message = response.choices[0].text;
-
-    // Update the HTML element with the response text
-    document.getElementById("response").innerHTML = message;
+function getResponse() {
+    // Get the value of the prompt input field
+    var prompt = $("#prompt").val();
+    
+    // Make an API request using the jQuery AJAX method
+    $.ajax({
+      url: "https://api.openai.com/v1/engines/text-davinci-002/completions",
+      type: "POST",
+      headers: {
+        "Authorization": "Bearer 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",
+        "Content-Type": "application/json"
+      },
+      data: JSON.stringify({
+        "prompt": prompt,
+        "max_tokens": 1024,
+        "temperature": 0.5
+      }),
+      success: function(response) {
+        // Extract the response text
+        var message = response.choices[0].text;
+        
+        // Update the HTML with the response text
+        $("#response").html(message);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
   }
