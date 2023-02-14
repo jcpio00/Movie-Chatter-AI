71f4cf5508e27c0802fc7c2caec4efa3e969b90e
Jcarpio <jcarpio1994@hotmail.com>
2023-02-13 19:05:23 -0500

    updated api and index.html

--- OpenAI_Api.js
+++ OpenAI_Api.js
@@ -30,25 +30,83 @@ function getResponse() {
 
   // search for movies based on a chosen genre using the TMDB API.
 
-  $(document).ready(function() {
-    var API_KEY = "5de60d88a55b9fbc648230694f21dd37";
-  
-    $("#search-btn").click(function() {
-      var genreId = $("#genres").val();
-      var url = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&with_genres=" + genreId;
+  var API_KEY = "5de60d88a55b9fbc648230694f21dd37";
 
-      //sending the request to the TMDB API and receiving a response in the form of a JSON object
+  $(document).ready(function() {
+    $(".categories-menu li").click(function() {
+      var genreId = 28; // initialize with a default genre id
+      var selectedCategory = $(this).text();
+  
+      // get the genre id for the selected category
+      switch (selectedCategory) {
+        case "Action":
+          genreId = 28;
+          break;
+        case "Comedy":
+          genreId = 35;
+          break;
+        case "Romance":
+          genreId = 10749;
+          break;
+        case "Musical":
+          genreId = 10402;
+          break;
+        case "Drama":
+          genreId = 18;
+          break;
+        case "Thriller":
+          genreId = 53;
+          break;
+        case "Horror":
+          genreId = 27;
+          break;
+        case "Crime":
+          genreId = 80;
+          break;
+        case "Documentaries":
+          genreId = 99;
+          break;
+        case "Fantasy":
+          genreId = 14;
+          break;
+        case "Sci-Fi":
+          genreId = 878;
+          break;
+        case "Animation":
+          genreId = 16;
+          break;
+        case "Fiction":
+          genreId = 9648;
+          break;
+        case "Non-Fiction":
+          genreId = 36;
+          break;
+        default:
+          genreId = 28;
+      }
+  
+      var url = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&with_genres=" + genreId;
   
       $.get(url, function(data, status) {
-        var movies = data.results;
+        var movies = data.results.slice(0,10);
         var movieList = "";
   
         for (var i = 0; i < movies.length; i++) {
-          movieList += "<p>" + movies[i].title + "</p>";
+          movieList += '<div class="movie-item col-md-3 col-sm-6">';
+          movieList += '<h2>' + movies[i].title + '</h2>';
+          movieList += '<p> Release Date: ' + movies[i].release_date + '</p>';
+          movieList += '<p> Overview: ' + movies[i].overview + '</p>';
+          movieList += '<p> Vote Average: ' + movies[i].vote_average + '</p>';
+          movieList += '</div>';
         }
   
         $("#movie-list").html(movieList);
       });
     });
   });
+  
+      
+  
+  
+
   
